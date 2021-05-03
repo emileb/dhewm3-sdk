1482f368b08adf76ae141948337b11aab67c43fe
Emile Belanger <emile.belanger@gmail.com>
2021-05-03 16:29:21 +0100

    Fix compile

--- mobile/Android_game.mk
+++ mobile/Android_game.mk
@@ -11,7 +11,7 @@ $(LOCAL_PATH)/mobile \
 $(LOCAL_PATH)/game \
 
 
-LOCAL_CPPFLAGS :=  -DGAME_DLL -DAIM_ASSIST -fPIC
+LOCAL_CPPFLAGS :=  -DGAME_DLL -DAIM_ASSIST  -DD3_OSTYPE=\"ANDROID\" -DD3_ARCH=\"ARM\" -fPIC
 
 LOCAL_CPPFLAGS += -std=gnu++11 -D__DOOM_DLL__ -frtti -fexceptions  -Wno-error=format-security
 
